99500fe1ae4de73f7ddbae819ba01de4c9e50a19
Wilmer Gelves <wfgelves.91@gmail.com>
2026-01-28 21:28:46 -0500

    Update TipoPrestamo model

--- backend/models/TipoPrestamo.ts
+++ backend/models/TipoPrestamo.ts
@@ -4,15 +4,17 @@ export interface TipoPrestamo {
   id_tipo_prestamo?: number;
   cantidad_cuotas: number;
   porcentaje: number;
+  nombre?: string;
 }
 
 // Crear un nuevo tipo de préstamo
 export const createTipoPrestamo = async (tipoPrestamo: TipoPrestamo): Promise<TipoPrestamo | null> => {
   const newTipoPrestamo = await db.query(
-    'INSERT INTO tipo_prestamo (cantidad_cuotas, porcentaje) VALUES ($1, $2) RETURNING *',
+    'INSERT INTO tipo_prestamo (cantidad_cuotas, porcentaje, nombre_tipo) VALUES ($1, $2, $3) RETURNING *',
     [
       tipoPrestamo.cantidad_cuotas,
-      tipoPrestamo.porcentaje
+      tipoPrestamo.porcentaje,
+      tipoPrestamo.nombre
     ]
   );
   return newTipoPrestamo.rows[0] || null;
@@ -39,10 +41,11 @@ export const deleteTipoPrestamo = async (id: number): Promise<TipoPrestamo | nul
 // Actualizar tipo de préstamo
 export const updateTipoPrestamo = async (id: number, tipoPrestamo: TipoPrestamo): Promise<TipoPrestamo | null> => {
   const updatedTipoPrestamo = await db.query(
-    'UPDATE tipo_prestamo SET cantidad_cuotas = $1, porcentaje = $2 WHERE id_tipo_prestamo = $3 RETURNING *',
+    'UPDATE tipo_prestamo SET cantidad_cuotas = $1, porcentaje = $2, nombre_tipo = $3 WHERE id_tipo_prestamo = $4 RETURNING *',
     [
       tipoPrestamo.cantidad_cuotas,
       tipoPrestamo.porcentaje,
+      tipoPrestamo.nombre,
       id
     ]
   );
