a0fe2505c66da5ca24445e3b68a5b43ede8dfe2b
Wilmer Gelves <wfgelves.91@gmail.com>
2026-02-07 21:38:23 -0500

    Update ruta routes

--- backend/routes/ruta.ts
+++ backend/routes/ruta.ts
@@ -3,7 +3,7 @@ import express from "express";
 
 const router = express.Router();
 router.post('/createRuta', ruta.createRuta);
-router.get('/getRutas', ruta.getRutas);
+router.get('/getRutas/:idSucursal', ruta.getRutas);
 router.get('/getRutaById/:id', ruta.getRutaById);
 router.put('/updateRuta/:id', ruta.updateRuta);
 router.delete('/deleteRuta/:id', ruta.deleteRuta);
